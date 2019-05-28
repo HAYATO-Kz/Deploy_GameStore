5db65a40e6222f322fd2dd863e0642dded703a00
JPSKE15 <hokiz_1999@hotmail.com>
2019-05-28 15:38:09 +0700

    template for index.html

---
+++ index.js
@@ -0,0 +1,13 @@
+function change(){
+    $("#loginModal").modal('hide');
+    $("#registerModal").modal('show');
+}
+
+function signUp(){
+
+}
+
+function test(){
+    var showGame = document.getElementById('showGame');
+    showGame.innerHTML += '<div class = "box" style="background-color: black"></div>'
+}
