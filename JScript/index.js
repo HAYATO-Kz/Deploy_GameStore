d01657f521c14b5ba65dbe326cd81018813091da
VichakornNewaz <viyotboon_new@outlook.com>
2019-05-29 18:48:13 +0700

    can register

--- JScript/index.js
+++ JScript/index.js
@@ -1,19 +1,71 @@
-function change(){
+function change() {
     $("#loginModal").modal('hide');
     $("#registerModal").modal('show');
 }
 
-function signUp(){
+function signUp() {
+    var firstName = $('#firstNameInput').val();
+    var lastName = $('#lastNameInput').val();
+    var age = $('#ageInput').val();
+    var email = $('#emailInput').val();
+    var password = $('#passwordInput').val();
+    var address = $('#addressInput').val();
+
+    var data = {
+        "name": {
+            "first_name": firstName,
+            "last_name": lastName
+        },
+        "age": age,
+        "email": email,
+        "password": password,
+        "address": address
+    };
+
+    var url = 'http://localhost:3000/user/signup';
+
+    $.ajax({
+        dataType: 'json',
+        url: url,
+        type: 'POST',
+        data: JSON.stringify(data),
+        contentType: 'application/json'
+    });
+
+    $("#registerModal").modal('hide');
 
 }
 
-function check(){
+function check() {
     var showGame = document.getElementById('showGame');
     showGame.innerHTML = '';
     showGame.innerHTML += '<div class = "box" style="background-color: black"></div>'
 }
 
-function search(){
-    
+function search() {
+
 }
 
+function login() {
+    var email = $('#inputID').val();
+    var password = $('#inputPassword').val();
+
+    var url = 'http://localhost:3000/user/login';
+
+    var data = {
+        "email": email,
+        "password": password
+    };
+
+    $.ajax({
+        dataType: 'json',
+        url: url,
+        type: 'POST',
+        data: JSON.stringify(data),
+        contentType: 'application/json'
+    });
+
+    $("#loginModal").modal('hide');
+
+
+}
